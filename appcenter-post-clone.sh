9d5a38ee34cc8467e81dd17a0bf8e7c51efc9862
Aaron Chancey <aaron.chancey@pavilionpay.com>
2023-12-08 16:35:18 -0500

    add appcenter

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -ex
-wget https://download.java.net/java/GA/jdk17/0d483333a00540d886896bac774ff48b/35/GPL/openjdk-17_osx-x64_bin.tar.gz
+wget https://download.oracle.com/java/17/archive/jdk-17.0.9_macos-x64_bin.tar.gz
 tar xvf openjdk-17_osx-x64_bin.tar.gz
 export JAVA_HOME=$PWD/jdk-17.jdk/Contents/Home
 export PATH=$JAVA_HOME/bin:$PATH
